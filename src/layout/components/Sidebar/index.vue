4c25cd2085354892c9325895330753962506b9df
xujian <2513573453@qq.com>
2022-03-28 00:50:28 +0800

    开发layout布局

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -2,12 +2,29 @@
  * @Author: xujian
  * @Date: 2022-03-27 12:27:09
  * @LastEditors: xujian
- * @LastEditTime: 2022-03-27 12:27:12
+ * @LastEditTime: 2022-03-28 00:49:40
  * @Description: 
  * @FilePath: /m-element-components/src/layout/components/Sidebar/index.vue
 -->
 <template>
-  <div class=""></div>
+  <el-menu
+    default-active="2"
+    class="el-menu-vertical-demo"
+    :collapse="isCollapse"
+  >
+    <el-menu-item index="2">
+      <el-icon-menu></el-icon-menu>
+      <span>Navigator Two</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <el-icon-document></el-icon-document>
+      <span>Navigator Three</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <el-icon-setting></el-icon-setting>
+      <span>Navigator Four</span>
+    </el-menu-item>
+  </el-menu>
 </template>
 
 <script setup>
